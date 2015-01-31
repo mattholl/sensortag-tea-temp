1d15b3e0fbe4b8eeae399cd7d68098a570553e01
Matthew Hollings <matthew.hollings@gmail.com>
2015-01-31 12:33:37 +0000

    Add a basic test for the sensortag connection.

--- test.js
+++ test.js
@@ -1,10 +1,18 @@
-exports.testSomething = function(test){
-    test.expect(1);
-    test.ok(true, "this assertion should pass");
-    test.done();
-};
-
-exports.testSomethingElse = function(test){
-    test.ok(false, "this assertion should fail");
-    test.done();
+var SensorTag = require("sensortag");
+exports.testConnect = function(test) {
+    test.expect(3);
+    SensorTag.discover(function(sensorTag) {
+        // console.log(sensorTag);
+        sensorTag.connect(function() {
+            sensorTag.discoverServicesAndCharacteristics(function(){
+                test.ok(true, "should be able to conect to the sensortag");
+                console.log('rssi : ' + sensorTag._peripheral.rssi);
+                test.ok(sensorTag.uuid);
+                console.log('UUID : ' + sensorTag.uuid);
+                test.ok(sensorTag.uuid);
+                // sensorTag.disconnect();
+                test.done();
+            });
+        });
+    });
 };
