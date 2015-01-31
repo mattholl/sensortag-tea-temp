23364fb5930f8dc67f6fe6f0ff0bc72a7816f854
Matthew Hollings <matthew.hollings@gmail.com>
2015-01-31 12:32:52 +0000

    Add a click handler to exit the node process when the notification balloon is clicked.

--- tea-time.js
+++ tea-time.js
@@ -1,62 +1,32 @@
-// var notify = require("osx-notifier"),
 var SensorTag = require("sensortag");
 var notifier = require('node-notifier');
 var nc = new notifier.NotificationCenter();
+var path = require('path');
+
+var idealTemp = 45;
 
 SensorTag.discover(function(sensorTag) {
-    console.log(sensorTag);
 
     sensorTag.connect(function() {
         sensorTag.discoverServicesAndCharacteristics(function(){
-            sensorTag.on('notifySimpleKey', function(left, right) {
-                 console.log(left, right);
-            });
-            sensorTag.on('simpleKeyChange', function(left, right) {
-                 console.log(left, right);
-            });
             sensorTag.enableIrTemperature();
-            sensorTag.notifyIrTemperature(function(objTemp, ambTemp) {
-                console.log('object temp : ' + objTemp);
-                console.log('ambient temp : ' + ambTemp);
-            });
-            sensorTag.on('irTemperatureChange', function(objTemp, ambTemp) {
-                console.log('object temp : ' + objTemp);
-                console.log('ambient temp : ' + ambTemp);
-                sensorTag._peripheral.updateRssi();
-                console.log(sensorTag._peripheral.rssi);
-            });
-            sensorTag.notifySimpleKey(function() {
-                console.log('simplekey notify correctly set up');
-            });
-            // sensorTag._peripheral._noble.updateRssi();
-            // sensorTag._peripheral._noble.on('rssiUpdate', function(rssi) {
-            //     console.log('rssi : ' + rssi);
-            // });
-            console.log(sensorTag._peripheral.rssi);
+            sensorTag.notifyIrTemperature(function() {});
         });
     });
 
-    sensorTag.on('simpleKeyChange', function(left, right) {
-        if (left) {
-            var temp = 45;
-            notify({
-              type: 'info',
-              title: 'tea\'s ready',
-              subtitle: 'Task completed',
-              message: 'Took ' + temp + ' seconds.',
-              group: 'taskdoer',
-            });
+    sensorTag.on('irTemperatureChange', function(objTemp, ambTemp) {
 
-        } else if (right) {
-          console.log('right pressed');
-          nc.notify({
-              title: 'My awesome title',
-              message: 'Hello from node, Mr. User!',
-              //icon: path.join(__dirname, 'coulson.jpg'), // absolute path (not balloons)
-              sound: 'Pop', // Only Notification Center or Windows Toasters
-              wait: true // wait with callback until user action is taken on notification
-            }, function (err, response) {
-              // response is response from notification
+        if(objTemp < idealTemp) {
+            nc.notify({
+                title: 'Tee ist fertig!',
+                message: 'The ideal temperature has been reached.',
+                icon: path.join(__dirname, 'serving1.png'), // absolute path (not balloons)
+                sound: 'Pop', // Only Notification Center or Windows Toasters
+                wait: true // wait with callback until user action is taken on notification
+            }, function() {
+                console.log('Tee ist fertig!');
+            }).on('click', function() {
+                process.exit(0);
             });
         }
     });
